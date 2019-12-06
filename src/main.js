f3a79ffe63d07d024a6a5dc4aa3f05ba4c64998e
Maria <reflection2008@gmail.com>
2019-12-06 15:36:58 +0300

    Кнопка Дозагрузить

--- src/main.js
+++ src/main.js
@@ -7,7 +7,10 @@ import {createMoreButton} from './components/more-button.js';
 import {generateFilters} from './mock/filter.js';
 import {generateTasks} from './mock/tasks.js';
 
-const TASK_COUNT = 8;
+const TASK_COUNT = 25;
+const TASK_SHOWING_ON_START = 8;
+const TASK_SHOWING_ON_LOAD_MORE = 8;
+
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -27,7 +30,21 @@ const taskListElement = siteMainElement.querySelector(`.board__tasks`);
 const tasks = generateTasks(TASK_COUNT);
 
 render(taskListElement, createTaskEditTemplate(tasks[0]), `beforeend`);
-tasks.slice(1).forEach((task) => render(taskListElement, createTaskTemplate(task), `beforeend`));
+let showingTasksCount = TASK_SHOWING_ON_START;
+tasks.slice(1, showingTasksCount).forEach((task) => render(taskListElement, createTaskTemplate(task), `beforeend`));
 
 const boardElement = siteMainElement.querySelector(`.board`);
 render(boardElement, createMoreButton(), `beforeend`);
+
+const loadMoreButton = boardElement.querySelector(`.load-more`);
+loadMoreButton.addEventListener(`click`, () => {
+  const prevTasksCount = showingTasksCount;
+  showingTasksCount = showingTasksCount + TASK_SHOWING_ON_LOAD_MORE;
+
+  tasks.slice(prevTasksCount, showingTasksCount)
+    .forEach((task) => render(taskListElement, createTaskTemplate(task), `beforeend`));
+
+  if (showingTasksCount >= tasks.length) {
+    loadMoreButton.remove();
+  }
+});
